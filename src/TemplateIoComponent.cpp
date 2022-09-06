2880a6940b29a0df4b062869651403a28821c37e
Georg Schwab <georg.schwab@xentara.io>
2022-09-06 17:06:49 +0200

    added sample code

---
+++ src/TemplateIoComponent.cpp
@@ -0,0 +1,121 @@
+// Copyright (c) embedded ocean GmbH
+#include "TemplateIoComponent.hpp"
+
+#include "Attributes.hpp"
+#include "TemplateIoBatch.hpp"
+#include "TemplateInput.hpp"
+#include "TemplateOutput.hpp"
+
+#include <xentara/data/ReadHandle.hpp>
+#include <xentara/model/Attribute.hpp>
+#include <xentara/plugin/SharedFactory.hpp>
+#include <xentara/utils/json/decoder/Object.hpp>
+#include <xentara/utils/json/decoder/Errors.hpp>
+
+#include <string_view>
+
+namespace xentara::plugins::templateDriver
+{
+
+using namespace std::literals;
+
+TemplateIoComponent::Class TemplateIoComponent::Class::_instance;
+
+auto TemplateIoComponent::loadConfig(const ConfigIntializer &initializer,
+		utils::json::decoder::Object &jsonObject,
+		config::Resolver &resolver,
+		const FallbackConfigHandler &fallbackHandler) -> void
+{
+	// Get a reference that allows us to modify our own config attributes
+    auto &&configAttributes = initializer[Class::instance().configHandle()];
+
+	// Go through all the members of the JSON object that represents this object
+	for (auto && [name, value] : jsonObject)
+    {
+		/// @todo load configuration parameters
+		if (name == u8"TODO"sv)
+		{
+			/// @todo parse the value correctly
+			auto todo = value.asNumber<std::uint64_t>();
+
+			/// @todo check that the value is valid
+			if (!"TODO")
+			{
+				/// @todo use an error message that tells the user exactly what is wrong
+				utils::json::decoder::throwWithLocation(value, std::runtime_error("TODO is wrong with TODO parameter of template I/O component"));
+			}
+
+			/// @todo set the appropriate member variables, and update configAttributes accordingly (if necessary) 
+		}
+		else
+		{
+			// Pass any unknown parameters on to the fallback handler, which will load the built-in parameters ("id", "uuid", and "children"),
+			// and throw an exception if the key is unknown
+            fallbackHandler(name, value);
+		}
+    }
+
+	/// @todo perform consistency and completeness checks
+	if (!"TODO")
+	{
+		/// @todo use an error message that tells the user exactly what is wrong
+		utils::json::decoder::throwWithLocation(jsonObject, std::runtime_error("TODO is wrong with template I/O component"));
+	}
+}
+
+auto TemplateIoComponent::createIo(const io::IoClass &ioClass, plugin::SharedFactory<io::Io> &factory)
+	-> std::shared_ptr<io::Io>
+{
+	if (&ioClass == &TemplateInput::Class::instance())
+	{
+		return factory.makeShared<TemplateInput>(*this);
+	}
+	else if (&ioClass == &TemplateOutput::Class::instance())
+	{
+		return factory.makeShared<TemplateOutput>(*this);
+	}
+
+	/// @todo add any other supported I/O point types
+
+	return nullptr;
+}
+
+auto TemplateIoComponent::createIoBatch(const io::IoBatchClass &ioClass, plugin::SharedFactory<io::IoBatch> &factory)
+	-> std::shared_ptr<io::IoBatch>
+{
+	if (&ioClass == &TemplateIoBatch::Class::instance())
+	{
+		return factory.makeShared<TemplateIoBatch>(*this);
+	}
+
+	/// @todo add any other supported I/O batch types
+
+	return nullptr;
+}
+
+auto TemplateIoComponent::resolveAttribute(std::u16string_view name) -> const model::Attribute *
+{
+	/// @todo resolve any attributes this class supports using model::Attribute::resolve
+
+	return nullptr;
+}
+
+auto TemplateIoComponent::readHandle(const model::Attribute &attribute) const noexcept -> data::ReadHandle
+{
+	/// @todo create read handles for any readable attributes this class supports
+
+	// Nothing found
+	return data::ReadHandle::Error::Unknown;
+}
+
+auto TemplateIoComponent::prepare() -> void
+{
+	/// @todo open the handle for the I/O device
+}
+
+auto TemplateIoComponent::cleanup() -> void
+{
+	/// @todo close the handle to the I/O device
+}
+
+} // namespace xentara::plugins::templateDriver
---
+++ src/TemplateIoComponent.hpp
@@ -0,0 +1,118 @@
+// Copyright (c) embedded ocean GmbH
+#pragma once
+
+#include "Attributes.hpp"
+#include "CustomError.hpp"
+
+#include <xentara/io/Component.hpp>
+#include <xentara/io/ComponentClass.hpp>
+#include <xentara/utils/tools/Unique.hpp>
+#include <xentara/utils/core/Uuid.hpp>
+
+#include <string_view>
+#include <functional>
+
+namespace xentara::plugins::templateDriver
+{
+
+using namespace std::literals;
+
+/// @brief A class representing a specific type of I/O component.
+/// @todo rename this class to something more descriptive
+class TemplateIoComponent final : public io::Component
+{
+private:
+	/// @brief A structure used to store the class specific attributes within an element's configuration
+	struct Config final
+	{
+		/// @todo Add custom config attributes
+	};
+	
+public:
+	/// @brief The class object containing meta-information about this element type
+	class Class final : public io::ComponentClass
+	{
+	public:
+		/// @brief Gets the global object
+		static auto instance() -> Class&
+		{
+			return _instance;
+		}
+
+	    /// @brief Returns the array handle for the class specific attributes within an element's configuration
+	    auto configHandle() const -> const auto &
+        {
+            return _configHandle;
+        }
+
+		/// @name Virtual Overrides for io::ComponentClass
+		/// @{
+
+		auto name() const -> std::u16string_view final
+		{
+			/// @todo change class name
+			return u"TemplateIoComponent"sv;
+		}
+	
+		auto uuid() const -> utils::core::Uuid final
+		{
+			/// @todo assign a unique UUID
+			return "bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb"_uuid;
+		}
+
+		/// @}
+
+	private:
+	    /// @brief The array handle for the class specific attributes within an element's configuration
+		memory::Array::ObjectHandle<Config> _configHandle { config().appendObject<Config>() };
+
+		/// @brief The global object that represents the class
+		static Class _instance;
+	};
+
+	/// @brief A handle used to access the I/O component
+	/// @todo implement a proper handle
+	class Handle final : private utils::tools::Unique
+	{
+	};
+
+	/// @brief Returns a handle to the I/O component
+	auto handle() const -> const Handle &
+	{
+		return _handle;
+	}
+
+	/// @name Virtual Overrides for io::Component
+	/// @{
+
+	auto createIo(const io::IoClass &ioClass, plugin::SharedFactory<io::Io> &factory) -> std::shared_ptr<io::Io> final;
+
+	auto createIoBatch(const io::IoBatchClass &ioClass, plugin::SharedFactory<io::IoBatch> &factory) -> std::shared_ptr<io::IoBatch> final;
+
+	auto resolveAttribute(std::u16string_view name) -> const model::Attribute * final;
+
+	auto readHandle(const model::Attribute &attribute) const noexcept -> data::ReadHandle final;
+
+	auto prepare() -> void final;
+
+	auto cleanup() -> void final;
+
+	/// @}
+
+protected:
+	/// @name Virtual Overrides for io::Component
+	/// @{
+
+	auto loadConfig(const ConfigIntializer &initializer,
+		utils::json::decoder::Object &jsonObject,
+		config::Resolver &resolver,
+		const FallbackConfigHandler &fallbackHandler) -> void final;
+
+	/// @}
+
+private:
+	/// @brief A handle to the I/O component
+	Handle _handle;
+};
+
+} // namespace xentara::plugins::templateDriver
